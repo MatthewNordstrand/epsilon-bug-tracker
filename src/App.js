81d8330bf5764bc5584ab4f109bc4065863adc29
Matthew Nordstrand <matt_nordstrand@live.com>
2021-07-21 18:17:45 -0400

    Added Login Modal and moved added username state to App

--- src/App.js
+++ src/App.js
@@ -3,16 +3,23 @@ import SideNav from './components/SideNav/SideNav';
 import LoginBar from './components/LoginBar';
 import IssuesCard from './components/Issues/IssuesCard';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-    return (
-        <div className="App">
-            <SideNav />
-            <TitleBar />
-            <LoginBar />
-            <IssuesCard />
-        </div>
-    );
+class App extends Component {
+    state = {
+        username: "Guest",
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <SideNav />
+                <TitleBar />
+                <LoginBar username={this.state.username} />
+                <IssuesCard />
+            </div>
+        )
+    };
 }
 
 export default App;
--- src/components/LoginBar.js
+++ src/components/LoginBar.js
@@ -1,13 +1,31 @@
-import { Button } from 'reactstrap';
+import { Button, Modal, ModalBody, ModalHeader } from 'reactstrap';
 import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSignInAlt } from '@fortawesome/free-solid-svg-icons';
 
 export default class LoginBar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            modal: false,
+        };
+    }
+
+    toggleModal() {
+        this.setState({ modal: !this.state.modal });
+    }
+
     render() {
         return (
             <div className="text-right mr-3 mt-1 mb-1">
-                Anonymous <Button size="sm"><FontAwesomeIcon icon={faSignInAlt}/> Login</Button>
+                {this.props.username} <Button size="sm" onClick={() => this.toggleModal()}><FontAwesomeIcon icon={faSignInAlt}/> Login{this.state.modal}</Button>
+
+                <Modal isOpen={this.state.modal} toggle={() => this.toggleModal()} className="loginModal">
+                    <ModalHeader toggle={() => this.toggleModal()}>Login</ModalHeader>
+                    <ModalBody>
+
+                    </ModalBody>
+                </Modal>
             </div>
         );
     }
