ebfcb51d89896da7427b12bebcb31fc9d25190fe
Matthew Nordstrand <matt_nordstrand@live.com>
2021-07-21 17:32:49 -0400

    Fixed siednav location

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ import './App.css';
 function App() {
     return (
         <div className="App">
-            <TitleBar />
             <SideNav />
+            <TitleBar />
             <LoginBar />
             <IssuesCard />
         </div>
