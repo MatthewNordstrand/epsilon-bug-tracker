8fc679fd332de43945ac2e6b42e989808e361eee
Matthew Nordstrand <matt_nordstrand@live.com>
2021-07-22 19:29:50 -0400

    Open Issue Modal started

--- src/components/Issues/IssuesButtonRow.js
+++ src/components/Issues/IssuesButtonRow.js
@@ -2,10 +2,14 @@ import React, { Component } from 'react';
 import { Button } from 'reactstrap';
 
 export default class IssuesButtonRow extends Component {
+    constructor(props) {
+        super(props);
+    }
+
     render() {
         return (
             <div className="mb-1">
-                <Button color="danger">Open Issue</Button>
+                <Button color="danger" onClick={this.props.openIssueModalToggle}>Open Issue</Button>
             </div>
         );
     }
--- src/components/Issues/IssuesCard.js
+++ src/components/Issues/IssuesCard.js
@@ -1,24 +1,42 @@
 import React, { Component } from 'react';
-import { Card, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardBody, CardTitle, Modal, ModalHeader, ModalBody } from 'reactstrap';
 import IssuesButtonRow from './IssuesButtonRow';
 import IssuesTable from './IssuesTable';
 
 export default class IssuesCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            openIssueModal: false,
+        };
+    }
+
+    openIssueModalToggle = () => this.setState({ openIssueModal: !this.state.openIssueModal });
+
     render() {
         return (
-            <div className="container-fluid">
-                <div className="row">
-                    <div className="col">
-                        <Card>
-                            <CardBody className="pt-0 pb-0">
-                                <CardTitle tag="h3" className="text-center">Issues</CardTitle>
-                                <IssuesButtonRow />
-                                <IssuesTable />
-                            </CardBody>
-                        </Card>
+            <>
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col">
+                            <Card>
+                                <CardBody className="pt-0 pb-0">
+                                    <CardTitle tag="h3" className="text-center">Issues</CardTitle>
+                                    <IssuesButtonRow openIssueModalToggle={() => this.openIssueModalToggle()} />
+                                    <IssuesTable />
+                                </CardBody>
+                            </Card>
+                        </div>
                     </div>
                 </div>
-            </div>
+
+                <Modal isOpen={this.state.openIssueModal} toggle={() => this.openIssueModalToggle()} className="open-issue-modal">
+                    <ModalHeader>Open Issue</ModalHeader>
+                    <ModalBody>
+                        Hello world!
+                    </ModalBody>
+                </Modal>
+            </>
         );
     }
 }
