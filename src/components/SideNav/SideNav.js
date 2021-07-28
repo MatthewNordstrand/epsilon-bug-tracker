c0d1c2ffaf49d41957c36a1cbe7a944ef0214c0c
Matthew Nordstrand <matt_nordstrand@live.com>
2021-07-27 20:14:35 -0400

    Sidebar navigation now works with router

--- src/components/SideNav/SideNav.js
+++ src/components/SideNav/SideNav.js
@@ -1,29 +1,41 @@
 import React from 'react';
-import { Drawer, List, ListItem, ListItemText } from '@material-ui/core';
+import { Drawer, ListItemText, MenuItem, MenuList } from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
+import { NavLink, withRouter } from 'react-router-dom';
+import Routes from '../../shared/Routes';
 
 const useStyles = makeStyles({
     list: {
         width: 250,
+    },
+    navLink: {
+        color: "inherit",
+        textDecoration: "none",
     }
 });
 
-export default function SideNav(props) {
+function SideNav(props) {
     const classes = useStyles();
 
+    const activeRoute = (routeName) => {
+        return props.location.pathname === routeName.slice(1);
+    }
+
     return (
-        <Drawer anchor="left" open={props.open} onClose={props.toggleDrawer}>
-            <List className={classes.list}>
-                <ListItem button>
-                    <ListItemText>Dashboard</ListItemText>
-                </ListItem>
-                <ListItem button selected>
-                    <ListItemText>Issues</ListItemText>
-                </ListItem>
-                <ListItem button>
-                    <ListItemText>Stats</ListItemText>
-                </ListItem>
-            </List>
+        <Drawer anchor="left" open={props.open} onClose={props.toggleDrawer} onClick={props.toggleDrawer}>
+            <MenuList className={classes.list}>
+                {Routes.map((prop, key) => {
+                    return (
+                        <NavLink to={prop.path} style={{ textDecoration: "none", color: "inherit" }}>
+                            <MenuItem selected={activeRoute(prop.path)}>
+                                <ListItemText primary={prop.sidebarName} />
+                            </MenuItem>
+                        </NavLink>
+                    );
+                })}
+            </MenuList>
         </Drawer>
     );
 }
+
+export default withRouter(SideNav);
---
+++ src/shared/Routes.js
@@ -0,0 +1,16 @@
+const Routes = [
+    {
+        path: './dashboard',
+        sidebarName: 'Dashboard',
+    },
+    {
+        path: './issues',
+        sidebarName: 'Issues',
+    },
+    {
+        path: './stats',
+        sidebarName: 'Stats',
+    },
+];
+
+export default Routes;
